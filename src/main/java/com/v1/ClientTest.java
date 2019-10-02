90d1e37be27dedc1c4aca595ffd8c38edea24a61
Pawan Mandhan <mandhanpawan7@gmail.com>
2019-10-02 20:31:52 +0530

    functional interface intro

---
+++ src/main/java/com/v1/ClientTest.java
@@ -0,0 +1,43 @@
+package com.v1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ClientTest {
+
+	public static void main(String[] args) {
+
+		// createThreadUsingAnonymousClass();
+		// createThreadUsingLamdaExpression();
+
+		List<String> nameList = new ArrayList<>();
+		nameList.add("Andrew");
+		nameList.add("Sean");
+		nameList.add("Rocky");
+
+		// nameList.forEach((name)-> System.out.println(name));
+		nameList.forEach(System.out::println);
+
+	}
+
+	private static void createThreadUsingLamdaExpression() {
+		Runnable r = () -> {
+			System.out.println("My task is executing..");
+		};
+		Thread thread = new Thread(r);
+		thread.start();
+	}
+
+	private static void createThreadUsingAnonymousClass() {
+		Runnable r = new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("My task is executing..");
+			}
+		};
+
+		Thread thread = new Thread(r);
+		thread.start();
+	}
+}
