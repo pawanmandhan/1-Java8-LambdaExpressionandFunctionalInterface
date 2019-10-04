1f7764d3c9f76438f70f6771224954fd7ddf6e9a
Pawan Mandhan <mandhanpawan7@gmail.com>
2019-10-04 15:48:58 +0530

    ReferenceToInstanceMethodofArbitraryObjectinLambdaExpressionDemo v14

---
+++ src/main/java/com/v14/ReferenceToInstanceMethodofArbitraryObjectinLambdaExpressionDemo.java
@@ -0,0 +1,27 @@
+package com.v14;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class ReferenceToInstanceMethodofArbitraryObjectinLambdaExpressionDemo {
+
+	public static void main(String[] args) {
+
+		List<Person> persons = new ArrayList<>();
+		persons.add(new Person("Bean", 27));
+		persons.add(new Person("Sean", 21));
+		persons.add(new Person("Martin", 45));
+		persons.add(new Person("Frank", 27));
+
+		List<String> personNames = ReferenceToInstanceMethodofArbitraryObjectinLambdaExpressionDemo
+				.getPersonsName(persons, Person::getName);
+		personNames.forEach(System.out::println);
+	}
+
+	private static List<String> getPersonsName(List<Person> persons, Function<Person, String> f) {
+		List<String> results = new ArrayList<>();
+		persons.forEach(person -> results.add(f.apply(person)));
+		return results;
+	}
+}
